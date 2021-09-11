a9e1ae8e9f1ca6c479e628a71fa8e511c94c3e45
Pauline3414159 <fagie37@hotmail.com>
2021-09-11 14:30:41 -0400

    edit values for needs category

--- budget_app.rb
+++ budget_app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 enable :sessions
 
@@ -9,6 +10,7 @@ before do
 end
 
 get '/' do
+
   @needs = (@income * 0.5)
   @wants =  (@income * 0.3)
   @savings = (@income * 0.2)
@@ -28,6 +30,13 @@ post '/new_needs' do
     session['needs_hash'] = {}
     session['needs_hash'][params['new_category']] = params['new_amount'].to_i
   end
+  redirect '/'
+end
+
+post '/edit_needs/:category' do
+  # binding.pry
+  # params
+  session['needs_hash'][params['category']] = params['new_val'].to_i
   redirect '/'
 end
 
