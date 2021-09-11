f4a8d887be2f72e61b0589e636d7bf1ddd8c2f9d
Pauline3414159 <fagie37@hotmail.com>
2021-09-11 13:57:07 -0400

    add needs total, and needs total over or under

--- budget_app.rb
+++ budget_app.rb
@@ -5,12 +5,14 @@ enable :sessions
 
 before do
   @income = session['income'] || 0
+  @needs_hash = session['needs_hash'] || {}
 end
 
 get '/' do
-  @needs = (@income * 0.5) || 0
-  @wants =  (@income * 0.3) || 0
-  @savings = (@income * 0.2) || 0
+  @needs = (@income * 0.5)
+  @wants =  (@income * 0.3)
+  @savings = (@income * 0.2)
+  @needs_values_total = @needs_hash.values.sum
   erb :main
 end
 
@@ -19,5 +21,15 @@ post '/income' do
   redirect '/'
 end
 
-# session {key: value}
+post '/new_needs' do
+  if session.key?('needs_hash')
+    session['needs_hash'][params['new_category']] = params['new_amount'].to_i
+  else
+    session['needs_hash'] = {}
+    session['needs_hash'][params['new_category']] = params['new_amount'].to_i
+  end
+  redirect '/'
+end
+
+# session {needs_hash: {housing: 300, ... }}
 # {income: input}
